06aa281329725c56dc132a1d8d10fc8e89ce1183
1amos9 <amosthibault19@gmail.com>
2022-07-11 15:31:15 +0200

    guess number

--- js/app.js
+++ js/app.js
@@ -99,3 +99,74 @@ window.addEventListener('click', () => {
   description.classList.add('hidden');
   overlayP.classList.add('hidden');
 });
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+  if (e.key === 'Enter') {
+    guessBox();
+  }
+  var alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    'Escape',
+  ];
+  for (let i = 0; i < alphabet.length; i++) {
+    if (e.key === alphabet[i] && !description.classList.contains('hidden')) {
+      description.classList.add('hidden');
+      overlayP.classList.add('hidden');
+    } else if (e.key === alphabet[i] && !modal.classList.contains('hidden')) {
+      closeModal();
+    } else if (e.key === alphabet[i] && !lost.classList.contains('hidden')) {
+      closeLost();
+    }
+  }
+});
